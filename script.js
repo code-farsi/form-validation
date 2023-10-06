2187b4aef01b514efd1155735ebd2684c919d7cf
Hamed Najand <hamed.najand@smile.fr>
2023-10-06 17:41:41 +0200

    chore: update

--- script.js
+++ script.js
@@ -4,85 +4,75 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 
-// Show input error message
-function showError(input, message) {
+const showError = (input, message) => {
   const formControl = input.parentElement;
   formControl.className = "form-control error";
+
   const small = formControl.querySelector("small");
   small.innerText = message;
-}
+};
 
-// Show success outline
-function showSuccess(input) {
+const showSuccess = (input) => {
   const formControl = input.parentElement;
   formControl.className = "form-control success";
-}
+};
 
-// Check email is valid
-function checkEmail(input) {
-  if (!checkRequired(input)) {
-    const re =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (re.test(input.value.trim())) {
-      showSuccess(input);
-    } else {
-      showError(input, "ایمیل معتبر نیست");
-    }
-  }
-}
-
-// Check required fields
-function checkRequired(input) {
+const checkRequired = (input) => {
   let isRequired = false;
-  if (input.value.trim() === "") {
-    showError(input, `${getFieldName(input)} اجباری است`);
+
+  if (input.value === "") {
+    showError(input, `${getFieldName(input)}  اجباری است`);
     isRequired = true;
   } else {
     showSuccess(input);
   }
 
   return isRequired;
-}
+};
 
-// Check input length
-function checkLength(input, min, max) {
+const checkLength = (input, min, max) => {
   if (!checkRequired(input)) {
-    if (input.value.length < min) {
+    if (input.value.length < min)
       showError(
         input,
         `${getFieldName(input)} باید دست کم ${min} کاراکتر داشته باشد`
       );
-    } else if (input.value.length > max) {
+    else if (input.value.length > max)
       showError(
         input,
         `${getFieldName(input)} نباید بیشتر از ${max} کاراکتر داشته باشد`
       );
-    } else {
+    else showSuccess(input);
+  }
+};
+
+const checkEmail = (input) => {
+  if (!checkRequired(input)) {
+    const re =
+      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (re.test(input.value.trim())) {
       showSuccess(input);
+    } else {
+      showError(input, `${getFieldName(input)} معتبر نیست`);
     }
   }
-}
+};
 
-// Check passwords match
-function checkPasswordsMatch(input1, input2) {
+const checkPasswordsMath = (input1, input2) => {
   if (!checkRequired(input2)) {
     if (input1.value !== input2.value) {
-      showError(input2, "گذرواژه ها با هم همخوانی ندارد");
+      showError(input2, "گذرواژه ها با هم همخوانی ندارند");
     }
   }
-}
+};
 
-// Get fieldname
-function getFieldName(input) {
-  return input.dataset.name;
-}
+const getFieldName = (input) => input.dataset.name;
 
-// Event listeners
-form.addEventListener("submit", function (e) {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   checkLength(username, 3, 15);
-  checkLength(password, 6, 25);
   checkEmail(email);
-  checkPasswordsMatch(password, password2);
+  checkLength(password, 6, 25);
+  checkPasswordsMath(password, password2);
 });
